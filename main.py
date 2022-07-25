7b3c44cf45f966af91431f564f2e7c8199c73d77
Kevin Rodríguez <kyrodriguez97@gmail.com>
2022-07-25 15:47:36 -0500

    Add pre-commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from typing import Union
 
 app = FastAPI()
 
@@ -7,8 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
